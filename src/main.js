be97568c5eb8354639cc24ea82dce5301b11d4b8
Jonathan Siler <jonathan@volanosolutions.com>
2020-01-04 09:15:58 -0600

    Update main.js for bootstrap4

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-require('bootstrap/js/modal.js');
-require('bootstrap/js/dropdown.js');
-require('bootstrap/js/tooltip.js');
-require('bootstrap/dist/css/bootstrap.css');
-require('font-awesome/css/font-awesome.css');
-require('summernote/dist/summernote.css');
-require('summernote');
+import Modal from 'bootstrap/js/dist/modal.js';
+import Dropdown from 'bootstrap/js/dist/dropdown.js';
+import Tooltip from 'bootstrap/js/dist/tooltip.js';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
+// for Bootstrap 3
+// import 'summernote';
+// import 'summernote/dist/summernote.css';
+
+// for Bootstrap 4
+import 'summernote/dist/summernote-bs4';
+import 'summernote/dist/summernote-bs4.css';
 
 $('.summernote').summernote();
